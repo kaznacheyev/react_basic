938075c2c89e3674fb89c87c6af632c0d58741f0
Yaroslav Kaznacheyev <kaznacheyev88@gmail.com>
2018-02-06 11:46:06 +0200

    added normalized data

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
 
 function Comment({comment}) {
 	return (
@@ -14,10 +15,16 @@ function Comment({comment}) {
 }
 
 Comment.propTypes = {
+	id: PropTypes.string.isRequired,
+	// from connect
 	comment: PropTypes.shape({
 		user: PropTypes.string.isRequired,
 		text: PropTypes.string.isRequired
 	}).isRequired
 }
 
-export default Comment
+export default connect((state, ownProps) => {
+	return {
+		comment: state.comments.find(comment => comment.id === ownProps.id)
+	}
+})(Comment)
